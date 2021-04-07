1830b59132a64831fd70b3e6d5f1774545054307
Noah Cindrich <noahcindrich@gmail.com>
2021-04-07 15:19:54 -0600

    final commit

---
+++ src/views/Cart.vue
@@ -0,0 +1,136 @@
+
+<template>
+
+<div class="wrapper">
+  <div class="cart-products">
+    <div class="cart-product" v-for="product in cart" :key="product.id">
+      <div class="info">
+        <h1>{{product.name}}</h1>
+        <p>{{product.country}}</p>
+      </div>
+      <div class="image">
+        <img :src="'/images/products/'+product.image">
+      </div>
+      <div class="price">
+        <h2>{{product.price}}</h2>
+        <button class="auto" @click="removeItem(product)">Remove From Cart</button>
+      </div>
+    </div>
+  </div>
+  <h2>{{cartEmpty}}</h2>
+</div>
+</template>
+
+<script>
+
+
+export default {
+
+  name: 'Cart',
+  components: {
+
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    },
+    cartEmpty() {
+      if (this.$root.$data.cart == 0) {
+        return "Cart Empty!";
+      }
+      else {
+        return "";
+      }
+    }
+  },
+  methods: {
+    removeItem(product) {
+      for (let i = 0; i < this.$root.$data.cart.length; ++i) {
+        if (product == this.$root.$data.cart.[i]) {
+          this.$root.$data.cart.splice(i,1);
+        }
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .title {
+    width: 100%;
+    text-align: center;
+  }
+
+  .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .cart-products {
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  .cart-product {
+    margin: 10px;
+    margin-top: 50px;
+    width: 200px;
+  }
+
+  .cart-product img {
+    border: 2px solid #333;
+    height: 250px;
+    width: 200px;
+    object-fit: cover;
+  }
+
+  .cart-product .image {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 5px;
+  }
+
+  .info {
+    background: #F2921D;
+    color: #000;
+    padding: 10px 30px;
+    height: 80px;
+  }
+
+  .info h1 {
+    font-size: 16px;
+  }
+
+  .info h2 {
+    font-size: 14px;
+  }
+
+  .info p {
+    margin: 0px;
+    font-size: 10px;
+  }
+
+
+  .price {
+    display: flex;
+  }
+
+  button {
+    height: 50px;
+    background: #000;
+    color: white;
+    border: none;
+  }
+
+  .auto {
+    margin-left: auto;
+  }
+    </style>
